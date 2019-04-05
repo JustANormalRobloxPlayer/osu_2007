e5532a0505c8b9f56ff81473a2d12ddd6982727c
lolno <40842246+JustANormalRobloxPlayer@users.noreply.github.com>
2019-04-05 11:50:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.get("/", (req, res) => {
 });
 app.post("/register", require("./handlers/register"));
 
-// osu! connections
 app.get("/web/osu-login.php", require("./handlers/osu_login"));
 
 app.post("/web/osu-submit.php", require("./handlers/osu_submit"));
 
 app.get("/web/osu-getscores.php", require("./handlers/osu_getscores"));
 
-app.listen(require("./config.json").site.port);
+app.listen(require("./config.json")
+    .site.port);
